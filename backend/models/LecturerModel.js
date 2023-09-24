d81bbe9d0f0fc0410030956322ed4b731e0e1e86
salmaep <salma.edyna.tif421@polban.ac.id>
2023-09-24 16:51:27 +0700

    update 1.0

--- backend/models/LecturerModel.js
+++ backend/models/LecturerModel.js
@@ -1,60 +1,23 @@
 import mongoose from "mongoose";
+import profileSchema from "./profile.model";
+import teachingHistorySchema from "./teaching-history.model";
+import educationHistorySchema from "./education-history.model";
+import researchSchema from "./research.model";
+import pkmSchema from "./pkm.model";
 
-// Skema untuk Teaching_history
-const teachingHistorySchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // ID unik untuk setiap teaching history
-  Institution: String,
-  Position: String,
-  Start_Date: Date,
-  End_Date: Date,
-});
-
-// Skema untuk Education_history
-const educationHistorySchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // ID unik untuk setiap education history
-  Degree: String,
-  institution: String,
-  Graduation_Date: Date,
-});
-
-// Skema untuk Research
-const researchSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // ID unik untuk setiap research
-  Title: String,
-  Publication_Date: Date,
-  Research_Publication: String,
-});
-
-// Skema untuk PKM
-const pkmSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // ID unik untuk setiap PKM
-  PKM_Tittle: String,
-  Year: Date,
-  Description: String,
-});
-
-// Skema untuk Profile
-const profileSchema = mongoose.Schema({
-  Date_of_birth: Date,
-  gender: String,
-  Bio: String,
-  Profile_Picture: String,
-});
-
-// Skema utama untuk Lecturer
 const lecturerSchema = mongoose.Schema({
-    Username : String,
-    Password : String,
-    Full_Name: String,
-    Email: String,
-    Phone_Number: String,
-    Position: String,
-    Major: String,
-    Profile: profileSchema,
-    Teaching_history: [teachingHistorySchema], // Array dari teaching history
-    Education_history: [educationHistorySchema], // Array dari education history
-    Research: [researchSchema], // Array dari research
-    PKM: [pkmSchema], // Array dari PKM
+  Username: String,
+  Password: String,
+  Full_Name: String,
+  Email: String,
+  Phone_Number: String,
+  Position: String,
+  Major: String,
+  Profile: profileSchema,
+  Teaching_history: [teachingHistorySchema],
+  Education_history: [educationHistorySchema],
+  Research: [researchSchema],
+  PKM: [pkmSchema],
 });
 
 export default mongoose.model('Lecturer', lecturerSchema);
--- backend/routes/LecturerRoute.js
+++ backend/routes/LecturerRoute.js
@@ -4,11 +4,7 @@ import {
     getLecturerById,
     saveLecturer,
     updateLecturer,
-    deleteLecturer,
-    getTeachingHistoryById,
-    saveTeachingHistory,
-    updateTeachingHistory,
-    deleteTeachingHistory,
+    deleteLecturer
 } from "../controllers/LecturerController.js";
 
 const router = express.Router();
@@ -18,14 +14,5 @@ router.get('/lecturers/:id', getLecturerById);
 router.post('/lecturers', saveLecturer);
 router.patch('/lecturers/:id', updateLecturer);
 router.delete('/lecturers/:id', deleteLecturer);
-
-// CRUD operasi untuk array document Teaching_history
-router.get('/lecturers/:lecturerId/teaching-history/:historyId', getTeachingHistoryById);
-router.post('/lecturers/:lecturerId/teaching-history', saveTeachingHistory);
-router.patch('/lecturers/:lecturerId/teaching-history/:historyId', updateTeachingHistory);
-router.delete('/lecturers/:lecturerId/teaching-history/:historyId', deleteTeachingHistory);
-
-// ... Anda dapat menambahkan rute dan fungsi
-
 
 export default router;
