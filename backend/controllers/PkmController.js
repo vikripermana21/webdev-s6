41abb724c06e093a62ab98ff51db4a143def63ca
GitGudwl <zahri.al.tif421@polban.ac.id>
2023-10-01 15:47:50 +0700

    Add controller and its routing

--- backend/controllers/PkmController.js
+++ backend/controllers/PkmController.js
@@ -1,69 +1,79 @@
 import PKM from '../models/PKMModel.js';
 
-export const createPKM = async (req, res) => {
+export const createPKM= async (req, res) => {
     try {
-        const pkm = await PKM.create(req.body);
-        res.status(201).json({ msg: "PKM Created", data: pkm });
+        const PKM = await PKM.create(req.body);
+        res.status(201).json({ msg: "PKM Created", data: PKM });
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Error occurred while creating PKM.' });
+        res.status(500).json({ error: 'Terjadi kesalahan dalam menginput data PKM.' });
     }
-};
+}
 
-export const getAllPKM = async (req, res) => {
+export const showAllPKM = async (req, res) => {
+    const { id_dosen } = req.params;
     try {
-        const pkms = await PKM.findAll();
-        res.status(200).json(pkms);
+        const response = await PKM.findAll(
+            {
+                where: { id_dosen: id_dosen }
+            }
+        );
+        res.status(200).json(response);
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Error occurred while fetching PKMs.' });
+        res.status(500).json({ error: 'Terjadi kesalahan dalam mengambil data PKM.' });
     }
-};
+}
 
 export const getPKMById = async (req, res) => {
-    const { id } = req.params;
+    const { id_dosen, id_pkm } = req.params;
     try {
-        const pkm = await PKM.findByPk(id);
-        if (!pkm) {
-            return res.status(404).json({ error: 'PKM not found' });
-        }
-        res.status(200).json(pkm);
+        const response = await PKM.findOne(
+            {
+                where: { id_dosen: id_dosen, id_pkm: id_pkm }
+            }
+        );
+        res.status(200).json(response);
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Error occurred while fetching PKM.' });
+        res.status(500).json({ error: 'Terjadi kesalahan dalam mengambil data PKM.' });
     }
-};
+}
 
 export const updatePKM = async (req, res) => {
-    const { id } = req.params;
     try {
-        const [updated] = await PKM.update(req.body, {
-            where: { id_pkm: id }
+        await PKM.update(req.body, {
+            where: {
+                id_pkm: req.params.id_pkm
+            }
         });
-        if (updated) {
-            const updatedPKM = await PKM.findByPk(id);
-            return res.status(200).json(updatedPKM);
-        }
-        return res.status(404).json({ error: 'PKM not found' });
+        res.status(201).json({ msg: "Berhasil update PKM" });
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Error occurred while updating PKM.' });
+        res.status(500).json({ msg: "Terjadi kesalahan saat mengupdate PKM" });
     }
-};
+}
 
 export const deletePKM = async (req, res) => {
-    const { id } = req.params;
     try {
-        const deleted = await PKM.destroy({
-            where: { id_pkm: id }
+        const id = req.params.id_pkm; // Ambil ID dari parameter URL
+
+        // Menggunakan klausa 'where' untuk menghapus PKM dengan ID yang sesuai
+        const data = await PKM.destroy({
+            where: {
+                id_pkm: id
+            }
         });
-        if (deleted) {
-            res.status(200).json({ msg: "PKM deleted successfully" });
+
+        if (data === 1) {
+            // Data berhasil dihapus (data === 1 menunjukkan satu baris dihapus)
+            res.status(200).json({ msg: "PKM berhasil dihapus" });
         } else {
-            res.status(404).json({ error: 'PKM not found' });
+            // Data tidak ditemukan
+            res.status(404).json({ msg: "PKM tidak ditemukan" });
         }
     } catch (error) {
         console.error(error.message);
-        res.status(500).json({ error: 'Error occurred while deleting PKM.' });
+        res.status(500).json({ msg: "Terjadi kesalahan saat menghapus PKM" });
     }
 };
