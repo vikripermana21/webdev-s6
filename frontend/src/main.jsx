e04a872293ad9f8c69f890cb90b8ff51819d0e52
GitGudwl <zahri.al.tif421@polban.ac.id>
2023-10-03 14:57:53 +0700

    Add code to show profile image

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import Register from "./pages/Register/index.jsx";
 import Dashboard from "./pages/Dashboard/index.jsx";
 import Dosen from "./pages/Dosen/index.jsx";
 import DetailDosen from "./pages/Dosen/detail.jsx";
+import AddDosen  from "./pages/Dosen/addDosen.jsx";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +35,10 @@ const router = createBrowserRouter([
     path: "/dashboard",
     element: <Dashboard />,
   },
+  {
+    path: "/dosen/addDosen",
+    element :  <AddDosen/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
